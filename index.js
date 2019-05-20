10eb6e4807dbfc72d4a8423be700d1b46369e611
Sky <l.abranches123@gmail.com>
2019-05-20 19:48:11 -0300

    vai

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+const delay = require('delay');
+async function nomeDaFuncao(){
+	console.log("Hello Wolrd!");
+	await delay(3000);
+	console.log("Hello 2");
+	
+}
+nomeDaFuncao();
